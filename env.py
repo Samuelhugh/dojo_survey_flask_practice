65cc591cb169da20187d232ff52b0b287fa53a83
samuel hughes <samuelhugh33@gmail.com>
2021-11-18 10:50:14 -0600

    Added Some Changes

---
+++ env.py
@@ -0,0 +1,1 @@
+key = 'agjngoabioe847jfenL843832098JFSKEFBUKFB43794375yhiubf'
--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask_app.controllers import dojos
+from env import key
 
 
 from flask_app import app
+app.secret_key = key
 
 
 if __name__ == '__main__':
